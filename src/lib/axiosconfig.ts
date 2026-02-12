e79002aa5f58924e142d22da70e8e86acdaba316
yashapex <104975493+yashapex@users.noreply.github.com>
2026-02-12 17:55:55 +0530

    Update API base URL to use HTTPS

--- src/lib/axiosconfig.ts
+++ src/lib/axiosconfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://shadow-backend-seven.up.railway.app/api",
+  baseURL: "https://shadow-backend-seven.up.railway.app/api",
   headers: {
     "Content-Type": "application/json",
   },
